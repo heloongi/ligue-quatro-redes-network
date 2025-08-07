060ee566455174b963b7d269e61b589db8c4f0f8
heloongi <r.heloisa@escolar.ifrn.edu.br>
2025-08-07 15:19:59 -0300

    aaa

--- ligue quatro/Assets/Scripts/NetworkUI.cs
+++ ligue quatro/Assets/Scripts/NetworkUI.cs
@@ -1,22 +1,33 @@
 using UnityEngine;
-using UnityEngine.UI;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using UnityEngine.UI;
+using TMPro;
 
 public class NetworkUI : MonoBehaviour
 {
     public Button hostButton;
     public Button clientButton;
+    public TMP_InputField ipInput;
 
     void Start()
     {
-        hostButton.onClick.AddListener(() =>
-        {
-            NetworkManager.Singleton.StartHost();
-        });
+        hostButton.onClick.AddListener(StartAsHost);
+        clientButton.onClick.AddListener(StartAsClient);
+    }
 
-        clientButton.onClick.AddListener(() =>
-        {
-            NetworkManager.Singleton.StartClient();
-        });
+    void StartAsHost()
+    {
+        NetworkManager.Singleton.StartHost();
+        Debug.Log("Host iniciado");
+    }
+
+    void StartAsClient()
+    {
+        string ip = ipInput.text.Trim();
+        var transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+        transport.SetConnectionData(ip, 7777); // Porta padrão
+        NetworkManager.Singleton.StartClient();
+        Debug.Log($"Tentando conectar ao host em {ip}:7777");
     }
 }
